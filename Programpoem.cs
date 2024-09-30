cf68bb42ed97de5404506c9b3f5accb1906dc37f
kislicinat <oolivi820@gmail.com>
2024-09-30 12:57:02 +0500

    Add files via upload

---
+++ Programpoem.cs
@@ -0,0 +1,21 @@
+﻿Console.ForegroundColor = ConsoleColor.Green; 
+Console.WriteLine("Незнакомка"); 
+ 
+Console.WriteLine(""); 
+ 
+Console.ForegroundColor = ConsoleColor.Yellow; 
+Console.WriteLine("По вечерам, над ресторанами,"); 
+Console.WriteLine("Туманный возух дик и глух."); 
+Console.WriteLine("И правит окриками пьяными"); 
+Console.WriteLine("Весенний и тлетворный дух."); 
+Console.WriteLine ("Вдали, над пылью переулочной,"); 
+Console.WriteLine ("Над скукой загородных дач,"); 
+Console.WriteLine ("Чуть золотится крендель булочный"); 
+Console.WriteLine ("И раздается детский плач."); 
+ 
+Console.WriteLine (""); 
+ 
+Console.ForegroundColor = ConsoleColor.Red; 
+Console.WriteLine ("А. А. Блок"); 
+Console.ResetColor ();
+

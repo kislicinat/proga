db0ebcb84d68ffd84c3be29d5de5e64fe56c3d17
kislicinat <oolivi820@gmail.com>
2024-10-01 10:03:57 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class RectangCalculatorV2
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите длину первого катета:");
+        double cathetus1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите длину второго катета:");
+        double cathetus2 = Convert.ToDouble(Console.ReadLine());
+
+
+        double hypotenuse = Math.Sqrt(cathetus1 * cathetus1 + cathetus2 * cathetus2);
+
+
+        double median = 0.5 * Math.Sqrt(2 * (cathetus1 * cathetus1 + cathetus2 * cathetus2) - hypotenuse * hypotenuse);
+
+        Console.WriteLine("Длина медианы, проведенной к гипотенузе: " + median);
+    }
+}
